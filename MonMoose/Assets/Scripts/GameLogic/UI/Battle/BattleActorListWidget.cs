5af87cd766d3a18b7ddeb552838fdfd31c2581bd
JinZhiLong <zhilong0606@sina.com>
2020-05-11 08:41:58 +0800

    Battle Main Panel

---
+++ MonMoose/Assets/Scripts/GameLogic/UI/Battle/BattleActorListWidget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleActorListWidget : UIComponent
+{
+    private GameObjectPool m_pool;
+
+    protected override void OnInit(object param)
+    {
+        base.OnInit(param);
+        m_pool = GetComponentToWidget<GameObjectPool>((int)EWidget.Pool);
+        m_pool.Init(OnActorItemInit);
+    }
+
+    private void OnActorItemInit(GameObjectPool.PoolObjHolder holder)
+    {
+        BattleActorItemWidget widget = holder.obj.AddComponent<BattleActorItemWidget>();
+        widget.Initialize(window);
+        holder.AddComponent()
+    }
+
+    private enum EWidget
+    {
+        Pool,
+    }
+}
---
+++ MonMoose/Assets/Scripts/GameLogic/UI/Battle/BattleMainPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMainPanel : UIWindow
+{
+    protected override void OnInit(object param)
+    {
+        base.OnInit(param);
+        
+    }
+}
